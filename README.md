5dabfe608983d691c739b7b1a2b082d672c07561
legion <legion@email.com>
2016-08-23 21:27:43 +0200

    initial commit, add model of Cell

---
+++ README.md
@@ -0,0 +1,4 @@
+# GameOfLife
+
+My own implementation of graphical presentation of Game of Life which creates a simple simulation of life cell's activity.
+The project is created using Model-Video-Controller pattern, although it's not a standard MVC model from Spring framework.
---
+++ src/main/java/pl/arek/lifegame/model/Cell.java
@@ -0,0 +1,44 @@
+package pl.arek.lifegame.model;
+
+public class Cell {
+    private boolean alive;
+
+    Cell(boolean alive) {
+        this.setAlive(alive);
+    }
+
+    Cell(Cell cell) {
+        this(cell.isAlive());
+    }
+
+    boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    boolean changeState(int neighboursCount) {
+        if (isAlive()) {
+            return changeAliveState(neighboursCount);
+        }
+        return changeDeadState(neighboursCount);
+    }
+
+    private boolean changeAliveState(int neighboursCount) {
+        if ((neighboursCount < 2) || (neighboursCount > 3) ) {
+            alive = false;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean changeDeadState(int neighboursCount) {
+        if (neighboursCount == 3) {
+            alive = true;
+            return true;
+        }
+        return false;
+    }
+}
